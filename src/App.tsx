6c7e02103e192a0cbad83f9b2aa8c4c11b48dc74
AkashReddy2003 <akepatiakashreddy@gmail.com>
2025-06-02 23:06:31 +0530

    added cert

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import WhyChooseUs from './components/WhyChooseUs';
@@ -19,13 +19,16 @@ import ProfilePage from './pages/ProfilePage';
 import CouseList from './components/CouseList';
 import BlogList from './components/BlogList';
 import BlogPost from './components/BlogPost';
-
+import cert from './assets/InvvezorCertificate.png';
 
 function HomePage() {
   useEffect(()=>{
     window.scrollTo(0,0);
     document.title="Invezoro | Bridging Knowledge and Innovation";
   })
+
+
+
   return (
     <>
       <Hero />
@@ -45,6 +48,23 @@ function HomePage() {
 }
 
 function App() {
+  const Cert = () => {
+  
+
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="bg-white p-8 rounded-lg shadow-lg max-w-md">
+          <h1 className="text-2xl font-bold mb-4 text-left">Certificate</h1>
+          <img src={cert} alt="Certificate" className="w-full h-auto mb-4" />
+          <p className="text-gray-700 text-left">Name: P. Mani</p>
+          <p className="text-gray-700 text-left">Role: Java Devoloper</p>
+          <p className="text-gray-700 text-left">Start Date: 01/05/2025 </p>
+          <p className="text-gray-700 text-left">End Date: 31/05/2025</p>
+          <p className="text-gray-700 text-left">Issued Date: 02/06/2025</p>
+        </div>
+      </div>
+    );
+  };
   return (
     <Router>
       <div className="min-h-screen bg-white">
@@ -57,6 +77,7 @@ function App() {
           <Route path="/profile" element={<ProfilePage/>} />
           <Route path='/blogs' element={<BlogList/>} />
           <Route path="/post/:id" element={<BlogPost />} />
+          <Route path="/certificate_702025100987" element={<Cert/>} />
         </Routes>
         <Footer />
       </div>
